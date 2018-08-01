23687ee843dd131aa52e76bac32354cb887f486f
xiaoxia <920634755@qq.com>
2018-08-01 15:45:27 +0800

    页面切换，分页器完成

--- routes/route.js
+++ routes/route.js
@@ -25,7 +25,18 @@ router.get('/service',function (req,res) {
 })
 
 router.get('/comment',function (req,res) {
-  res.send(comments)
+     if(req.query.page){//如果有分页
+       var page=req.query.page*1;//把字符串转换为number型
+       var comment=comments.slice((page)*10,(page+1)*10)
+       //判断符合条件，返回新的数据
+       res.send(comment)
+     }else{
+        var arr=[]
+       for (var i = 1; i <=comments.length/10; i++) {
+         arr.push(i)
+       }
+       res.send(arr)
+     }
 })
 
 
